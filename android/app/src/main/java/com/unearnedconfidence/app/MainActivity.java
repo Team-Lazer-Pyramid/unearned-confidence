e2ae8087f29a21861ec03d47c213aafa9a82bd4d
Derek Niemuth <niemuth.derek@gmail.com>
2021-11-20 01:39:51 -0600

    initial app

---
+++ android/app/src/main/java/com/unearnedconfidence/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.unearnedconfidence.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { IonButton, IonDatetime } from '@ionic/react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <IonDatetime displayFormat="MM/DD/YYYY" placeholder="Select Date"></IonDatetime>
+      <IonButton fill="clear" onClick={() => console.log('Bekken Ja Goosch')}>Start</IonButton> 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
