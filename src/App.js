e94673950c070a2952a7ae3638568037edfb0aae
Derek Niemuth <niemuth.derek@gmail.com>
2021-11-20 22:14:03 -0600

    quick change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import { IonButton, IonDatetime } from '@ionic/react';
+import { CharacterList } from './pages/CharacterList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <IonDatetime displayFormat="MM/DD/YYYY" placeholder="Select Date"></IonDatetime>
-      <IonButton fill="clear" onClick={() => console.log('Bekken Ja Goosch')}>Start</IonButton> 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CharacterList />
       </header>
     </div>
   );
